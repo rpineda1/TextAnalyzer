d585a440922d2da68dcaabdb4cfe5bc496d7d43a
rpineda1 <rpineda1@mail.valenciacollege.edu>
2022-06-26 17:51:57 -0400

    Add test class

---
+++ MainControllerTest.java
@@ -0,0 +1,15 @@
+package application;
+
+
+
+import org.junit.jupiter.api.Test;
+
+class MainControllerTest {
+
+	@Test
+	public void testForWord() {
+		String word = "the";
+		boolean isValidWord = MainController.applicationButtonClicked(word "the");
+	}
+
+}
